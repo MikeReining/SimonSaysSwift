1522ce78a63029a9146c96acdb9f107e4384581e
Mike Reining <emailmike@gmail.com>
2015-01-21 09:34:48 -0800

    Implemented 4 UI Buttons with support for iPhone in Portrait and Landscape

--- SimonSaysSwift/ViewController.swift
+++ SimonSaysSwift/ViewController.swift
@@ -9,9 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var orangeButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        blueButton.backgroundColor = UIColor.blueColor()
+        blueButton.alpha = 0.5
+        greenButton.backgroundColor = UIColor.greenColor()
+        greenButton.alpha = 0.5
+        redButton.backgroundColor = UIColor.redColor()
+        redButton.alpha = 0.5
+        orangeButton.backgroundColor = UIColor.orangeColor()
+        orangeButton.alpha = 0.5
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
